b389b3f0f974caa0658918777e14b856d8bd28bc
Hanmanth26 <152951536+Hanmanth26@users.noreply.github.com>
2025-03-09 10:48:10 +0000

    comparission is done

---
+++ 01_basics/05_conditions.js
@@ -0,0 +1,27 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null > 0);
+console.log(null == 0); 
+console.log(null >= 0);
+
+//The reason is that an equality check == and  comparisons >< >= <= work differently.
+// comparisons convert null to a number, treating it as 0
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//=== strickely check
+
+console.log("2"=== 2);
+
+
+
