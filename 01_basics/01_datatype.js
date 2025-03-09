c315df6d7fad2a4bcea85620693f89c3ec4084d2
Hanmanth26 <152951536+Hanmanth26@users.noreply.github.com>
2025-03-09 09:50:57 +0000

    datastypes

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,25 @@
+"use strict";// treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)// code readability should be high
+
+console.log("Ajay")
+
+let name = "Ajay" //string
+let age = 18
+let isLoggedIn= false
+
+//number =>2 to power 53
+// bigint=>""
+// boolean =>true/false {let isLoggedIn= false}
+// null => standalone value {let state=null}
+// undeg=fined => dosent value defined {let state;} 
+// symbol => unique values
+
+//boject =>
+
+console.log(typeof 18);//number
+console.log(typeof "jay");//String
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
---
+++ 01_basics/02_variables.js
@@ -0,0 +1,13 @@
+accountEmail = "aj@ay.com"
+accountPassword = "123456"
+accountCity = "Bangaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to user car
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
